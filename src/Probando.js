bd73d4e204e6b6887e38006807361e8ad264852f
Dadario23 <dandrada23@gmail.com>
2023-01-28 03:39:37 -0300

    refactor(cleaning): format code by script

--- src/Probando.js
+++ src/Probando.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React from 'react'
 
-export default function Probando(){
-    const marcas = ['SAMSUNG','MOTOROLA','IPHONE','LG','ALCATEL','XIAOMI','NOBLEX','BGH','PCBOX']
-return (
+export default function Probando() {
+  const marcas = [
+    'SAMSUNG',
+    'MOTOROLA',
+    'IPHONE',
+    'LG',
+    'ALCATEL',
+    'XIAOMI',
+    'NOBLEX',
+    'BGH',
+    'PCBOX',
+  ]
+  return (
     <div>
-        <select name="marcas" id=''>
-            <option value="">Seleccione</option>
-            {marcas.map((item)=><option key={item} value={item}>{item}</option>)}
-        </select>
+      <select name='marcas' id=''>
+        <option value=''>Seleccione</option>
+        {marcas.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
     </div>
-)
+  )
 }
-
-
