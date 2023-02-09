5b6431065492c49f0ae70f255706c25ffcfb0e31
Dadario23 <dandrada23@gmail.com>
2023-02-09 19:45:13 -0300

    modificando tamaño de letra

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { Box } from "@mui/material";
 import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
